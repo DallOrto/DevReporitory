0900f830d4bfbcdb508b7bfe8a569ce67977294f
DallOrto <rafaeldallorto@gmail.comm>
2022-06-29 17:07:39 -0300

    feat:configuring aws

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,7 @@ import createConnection from "@shared/infra/typeorm";
 import "@shared/container";
 import { router } from "./routes";
 import { AppError } from "@shared/errors/AppError";
+import upload from "config/upload";
 
 
 
@@ -16,6 +17,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
