e9d8f82078caec729c29b4f178086b490c163290
DallOrto <rafaeldallorto@gmail.comm>
2022-07-05 14:06:21 -0300

    remove js files

--- src/modules/rentals/dtos/ICreateRentalDTO.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ICreateRentalDTO = void 0;
-var ICreateRentalDTO = /** @class */ (function () {
-    function ICreateRentalDTO() {
-    }
-    return ICreateRentalDTO;
-}());
-exports.ICreateRentalDTO = ICreateRentalDTO;
